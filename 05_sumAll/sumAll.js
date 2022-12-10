c4ebe1269335239c9244aa9bdad9ffab7dcc1894
tada <rortheford@gmail.com>
2022-12-09 17:59:59 -0800

    Solution to Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let i = num1;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    }
+    while (i !== num2) {
+        sum += i;
+        if (num1 > num2) 
+            i--;
+        else
+            i++;
+    }
+    sum += i;
+    return sum;
 };
 
 // Do not edit below this line
