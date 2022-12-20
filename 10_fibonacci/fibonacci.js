7b584a73dd75a5daa7f36b269bf0d5bae9f964f8
tada <rortheford@gmail.com>
2022-12-19 23:21:13 -0800

    Solution to Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof(num) === 'string')
+        num = parseInt(num);
+    if (num < 0) 
+        return "OOPS";
+    if (num <= 1)
+        return num;
+    return fibonacci(num-1) + fibonacci(num-2);
 };
 
 // Do not edit below this line
