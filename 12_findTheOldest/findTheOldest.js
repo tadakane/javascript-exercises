44e785c664caa6e08143d262b2570e6995dcb921
tada <rortheford@gmail.com>
2022-12-20 00:13:29 -0800

    Solution to Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const ages = arr.reduce((oldest, person) => {
+        if (!oldest.yearOfDeath)
+            oldest.yearOfDeath = new Date().getFullYear();
+        if (!person.yearOfDeath)
+            person.yearOfDeath = new Date().getFullYear();
+        return oldest.yearOfDeath - oldest.yearOfBirth > person.yearOfDeath - person.yearOfBirth ? 
+                                    oldest : person;
+    });
+    return ages;
 };
 
 // Do not edit below this line
