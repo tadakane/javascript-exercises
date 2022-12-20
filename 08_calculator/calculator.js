ef97b15d0e7dd93184f38fdb452646de7c04402d
tada <rortheford@gmail.com>
2022-12-19 22:53:15 -0800

    Solution to Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
 const add = function() {
-	
+	const args = Array.from(arguments);
+  const sum = args.reduce((total, current) => {
+    return total + current;
+  });
+  return sum;
 };
 
 const subtract = function() {
-	
+	const args = Array.from(arguments);
+  const diff = args.reduce((total, current) => {
+    return total - current;
+  });
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let totalSum = 0;
+  if (arr.length !== 0) {
+    totalSum = arr.reduce((total, current) => {
+      return total + current;
+    });
+  }
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 0;
+  if (arr.length !== 0) {
+    product = arr.reduce((total, current) => {
+      return total * current;
+    });
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  let result = 1;
+	for (let i = 0; i < exp; i++)
+    result = result * num;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = 1; i < num+1; i++)
+    result = result * i;
+  return result;
 };
 
 // Do not edit below this line
